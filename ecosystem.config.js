fee61d35583ac5fa89f8e678326fe8819cf0b445
diyachoudhury <diya@silfratech.com>
2026-04-10 09:52:23 +0530

    updated ecosystem config files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,19 +2,18 @@ module.exports = {
   apps: [
     {
       name: "libretranslate",
-      interpreter: "python3",
-      script: "-m",
-      args: "libretranslate --host 0.0.0.0 --port 5011 --load-only en,es",
+      script: "/home/apps/varchaswi/miraq-chat-wip/.venv/bin/python3",
+      args: "-m libretranslate --host 0.0.0.0 --port 5011 --load-only en,es",
       watch: false,
       autorestart: true,
       max_restarts: 5,
       restart_delay: 5000,
       env: {
-        PYTHONUNBUFFERED: "1", // ensures logs stream in real-time
+        PYTHONUNBUFFERED: "1",
       },
-      //   error_file: "logs/libretranslate-error.log",
-      //   out_file: "logs/libretranslate-out.log",
-      //   log_date_format: "YYYY-MM-DD HH:mm:ss",
+      error_file: "logs/libretranslate-error.log",
+      out_file: "logs/libretranslate-out.log",
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
     },
     {
       name: "chatbot-wip",
@@ -22,13 +21,6 @@ module.exports = {
       script: "server.py",
       watch: false,
       autorestart: true,
-      //   env: {
-      //     FLASK_ENV: "production",
-      //     PYTHONUNBUFFERED: "1",
-      //   },
-      //   error_file: "logs/chat-error.log",
-      //   out_file: "logs/chat-out.log",
-      //   log_date_format: "YYYY-MM-DD HH:mm:ss",
     },
   ],
 };
