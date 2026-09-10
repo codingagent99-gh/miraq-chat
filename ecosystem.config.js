976a132c806dd0ef29ba44cbe35fe41f31a310f9
diyachoudhury <diya@silfratech.com>
2026-09-10 14:26:34 +0530

    uplaoded ecosystem args

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: "miraq-chat-staging",
       script: "/home/apps/varchaswi/miraq-chat/.venv/bin/gunicorn",
       interpreter: "none",
-      args: "server:app --bind 0.0.0.0:5009 --workers 4 --worker-class gthread --threads 4 --timeout 120 --access-logfile - --log-level info",
+      args: "server:app --bind 0.0.0.0:5015 --workers 4 --worker-class gthread --threads 4 --timeout 120 --access-logfile - --log-level info",
       cwd: "/home/apps/varchaswi/miraq-chat-staging",
       instances: 1,
       autorestart: true,
