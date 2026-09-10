d9f039f386eec389c1dd6fb6e61286c1013d5b45
diyachoudhury <diya@silfratech.com>
2026-09-10 14:22:55 +0530

    uplaoded ecosystem path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "miraq-chat-staging",
-      script: "/home/apps/varchaswi/miraq-chat-staging/.venv/bin/gunicorn",
+      script: "/home/apps/varchaswi/miraq-chat/.venv/bin/gunicorn",
       interpreter: "none",
       args: "server:app --bind 0.0.0.0:5009 --workers 4 --worker-class gthread --threads 4 --timeout 120 --access-logfile - --log-level info",
       cwd: "/home/apps/varchaswi/miraq-chat-staging",
